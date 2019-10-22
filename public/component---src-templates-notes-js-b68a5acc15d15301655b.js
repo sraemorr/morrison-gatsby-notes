51fa8ce14c12d9c5c9eb161ba9621e729ca6767a
Steph Morrison <stephanie.r.morrison@gmail.com>
2019-10-22 00:55:31 -0700

    added public folder

---
+++ public/component---src-templates-notes-js-b68a5acc15d15301655b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{212:function(e,n,t){"use strict";t.r(n),t.d(n,"query",(function(){return o}));var a=t(0),r=t.n(a),l=t(211);n.default=function(e){var n=e.data.markdownRemark;console.log(n);var t=n.frontmatter.title;return r.a.createElement(l.a,null,r.a.createElement("h1",null,t),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:n.html}}))};var o="2663108596"}}]);
+//# sourceMappingURL=component---src-templates-notes-js-b68a5acc15d15301655b.js.map
